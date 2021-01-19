51ca367dacdafb05c9bcf127f2caceb130b80ec3
Immamura <77626976+Immamura@users.noreply.github.com>
2021-01-19 12:14:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    document.getElementById("viewport").setAttribute("content", "width=device-width, initial-scale=0, maximum-scale=1.0, minimum-scale=0.25, user-scalable=yes");
+	document.documentElement.style.setProperty('height', '100vh');
   } else {
     void(0)
 }
