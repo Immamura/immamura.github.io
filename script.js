546de9a00c557c0260b0d832813b37cd7f21208f
Immamura <77626976+Immamura@users.noreply.github.com>
2021-01-20 12:45:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 	syn.style.fontSize = "30px";
 	var syn0=document.getElementById('button')
 	syn0.style.left="51%";
-	syn0.style.top="65%";
+	syn0.style.top="67%";
 	var syn1=document.getElementById('contact')
-	syn1.style.left="23%";
-	syn1.style.top="48%";
+	syn1.style.left="30%";
+	syn1.style.top="52%";
 	
   } else {
     void(0)
